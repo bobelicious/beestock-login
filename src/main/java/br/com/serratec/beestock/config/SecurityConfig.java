57b83a3c75a032ebe7208a101f0bf670687d40d3
bobelicious <augusto.marley97@gmail.com>
2022-01-04 17:54:28 -0300

    tentativa2 do conserto do cors

--- src/main/java/br/com/serratec/beestock/config/SecurityConfig.java
+++ src/main/java/br/com/serratec/beestock/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
 	public	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/","http://localhost:3030/"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
--- src/main/java/br/com/serratec/beestock/controller/ProfileController.java
+++ src/main/java/br/com/serratec/beestock/controller/ProfileController.java
@@ -1,7 +1,6 @@
 package br.com.serratec.beestock.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.serratec.beestock.model.Profile;
-import br.com.serratec.beestock.repository.ProfileRepository;
 import br.com.serratec.beestock.service.ProfileService;
 @CrossOrigin(origins = {"*"})
 @RestController
