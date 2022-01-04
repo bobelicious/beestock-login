c302be09d7e72c7f5edeb16d788ffeee7fc236c7
bobelicious <augusto.marley97@gmail.com>
2022-01-04 18:49:21 -0300

    tentativa de conserto do cors parte 4

--- src/main/java/br/com/serratec/beestock/config/CorsConfiguration.java
+++ src/main/java/br/com/serratec/beestock/config/CorsConfiguration.java
@@ -8,7 +8,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
-        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS","HEAD","TRACE","CONNECT");
+        .allowedOrigins("http://localhost:3000","http://localhost:3030")
+        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS","HEAD","TRACE","CONNECT")
+        .allowCredentials(true);
     }
 }
--- src/main/java/br/com/serratec/beestock/config/SecurityConfig.java
+++ src/main/java/br/com/serratec/beestock/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000/","http://localhost:3030/"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","HEAD"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
