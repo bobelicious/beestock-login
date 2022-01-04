28c77e0c175d10cbf7dc2999a6d18517f8dcf4d9
bobelicious <augusto.marley97@gmail.com>
2022-01-04 18:55:42 -0300

    tentativa 5

--- src/main/java/br/com/serratec/beestock/controller/ProfileController.java
+++ src/main/java/br/com/serratec/beestock/controller/ProfileController.java
@@ -1,7 +1,6 @@
 package br.com.serratec.beestock.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.serratec.beestock.model.Profile;
 import br.com.serratec.beestock.service.ProfileService;
-@CrossOrigin(origins = {"*"})
+
 @RestController
 @RequestMapping("/profiles")
 public class ProfileController {
